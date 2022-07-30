00edc7a23b6de896c60490e0b93f8d9fe9ba8271
Enrickyb <enrickyb@hotmail.com>
2022-07-30 16:02:34 -0400

    refactor: cards

---
+++ src/data/projects.js
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    name: "Pokedex",
+    description:
+      "Project developed in NextJs and StyledComponents using the official pokemon API. In this project I had the opportunity to learn a little about pagination and API consumption in general",
+    image: "/images/pokedex.png",
+    techs: ["NextJs"],
+    deploy: "https://pokedex-enricky.netlify.app/",
+    repo: "https://github.com/Enrickyb/Pokedex",
+  },
+  {
+    name: "Netflix Clone",
+    description:
+      "This is a clone of the netflix main page. This was a challenging project where I could improve and put my knowledge into practice.",
+    image: "/images/NetflixClone.png",
+    techs: ["ReactJs", "CSS"],
+    deploy: "https://netflixbyenricky.netlify.app/",
+    repo: "https://github.com/Enrickyb/NetflixClone",
+  },
+  {
+    name: "Finance Control",
+    description:
+      "This project was developed to learn a little more about React mainly from React Hooks. It is possible to add monthly expenses and earnings.",
+    image: "/images/sistemafinanceiro.png",
+    techs: ["ReactJs"],
+    deploy: "https://github.com/Enrickyb/Controle-de-Financas",
+    repo: "https://github.com/Enrickyb/Controle-de-Financas",
+  },
+  {
+    name: "Via Certa",
+    description:
+      "This project is a clone of the website of the distributor Via Certa from Manaus-AM, in which I learned important lessons such as the image carousel and the Hamburger menu.",
+    image: "/images/viacerta.png",
+    techs: ["HTML", "CSS", "JavaScript"],
+    deploy: "https://enrickyb.github.io/Clone-site-ViaCerta/",
+    repo: "https://github.com/Enrickyb/Clone-site-ViaCerta",
+  },
+  {
+    name: "Tic Tac Toe",
+    description:
+      "A small project where I put into practice my JavaScript learnings and I could learn many things.",
+    image: "/images/memorygame.png",
+    techs: ["HTML", "CSS", "JavaScript"],
+    deploy: "https://enrickyb.github.io/projeto-jogo-da-velha/",
+    repo: "https://github.com/Enrickyb/projeto-jogo-da-velha",
+  },
+];
